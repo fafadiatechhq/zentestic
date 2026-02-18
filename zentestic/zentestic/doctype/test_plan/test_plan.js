29ef5478d397bd70d656bdadf42a378058b4b027
ftech <himanshu.maurya@fafadiatech.com>
2026-02-18 09:42:39 +0530

    ref: first itteration

--- zentestic/zentestic/doctype/test_plan/test_plan.js
+++ zentestic/zentestic/doctype/test_plan/test_plan.js
@@ -6,3 +6,29 @@
 
 // 	},
 // });
+frappe.ui.form.on('Test Plan', {
+    refresh(frm) {
+        if (!frm.is_new()) {
+            frm.add_custom_button('Start Test Run', function () {
+
+                // Create new Test Run document
+                frappe.new_doc('Test Run', {
+                    test_plan: frm.doc.name,
+                    project: frm.doc.project,
+                    product: frm.doc.product
+                }, function (doc) {
+
+                    // Copy Test Cases into Test Run child table
+                    frm.doc.test_cases.forEach(function (row) {
+                        let child = frappe.model.add_child(doc, "Test Result", "test_results");
+                        child.test_case = row.test_case;
+                    });
+
+                    frappe.set_route('Form', 'Test Run', doc.name);
+                });
+
+            });
+        }
+    }
+});
+
